c2b0d139b651a80c8824204d966a9746bb73923d
Akihiro Iwata <iakihiro22@gmail.com>
2019-04-28 11:53:42 +0900

    initial commit

---
+++ src/main/java/com/muscle/MslMgtApplication.java
@@ -0,0 +1,13 @@
+package com.muscle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MslMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MslMgtApplication.class, args);
+	}
+
+}
